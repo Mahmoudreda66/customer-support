40edaee0f3f410c9f4fbc7fbb59fe8674af64c7c
Mahmoud Reda <mahmodreda219@gmail.com>
2025-02-17 22:17:53 +0200

    queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('queue:retry all')
+            ->everyMinute()
+            ->withoutOverlapping();
+
+        $schedule->command('queue:work --stop-when-empty')
+            ->everyMinute()
+            ->withoutOverlapping();
     }
 
     /**
